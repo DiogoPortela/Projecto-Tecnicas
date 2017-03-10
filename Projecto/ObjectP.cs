8aef25f14c7f98d7197ea3ada6b6e403e1dc3de3
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2017-03-10 12:22:31 +0000

    Class Object

---
+++ Projecto/ObjectP.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projecto
+{
+    public class ObjectP
+    {
+        public Texture2D Image;
+        public Vector2 Position;
+        public Vector2 Size;
+                
+        public ObjectP(Texture2D img, Vector2 pos, Vector2 size)
+        {
+            this.Image = img;
+            this.Position = pos;
+            this.Size = size;
+        } 
+    }
+}
