5b7109c18e05d98971d0bb668d71583fd195bf13
Pedro Silva <pmiguelfs@gmail.com>
2017-05-16 19:22:23 +0100

    UPDATE

--- Projecto/Characters/Enemy.cs
+++ Projecto/Characters/Enemy.cs
@@ -60,28 +60,3 @@ namespace Projecto
     }
 }
 
-namespace Projecto
-{
-    public class Enemy
-    {
-        public int HP { get; set; }
-
-        public int MP { get; set; }
-
-        public float MS { get; set; }
-
-        public double PhysDmg { get; set; }
-
-        public double MagicDmg { get; set; }
-
-        public Enemy()
-        {
-            this.HP = 30;
-            this.MP = 30;
-            this.MS = 0.8f;
-            this.PhysDmg = 10;
-            this.MagicDmg = 5;
-        }
-
-    }
-}
--- Projecto/Graphics/Tile.cs
+++ Projecto/Graphics/Tile.cs
@@ -1,50 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Content;
-
-namespace Projecto
-{
-    internal class Tile : GameObject, SettlersEngine.IPathNode<Object>
-    {
-        public Vector2 Coordinates;
-        public Collider Collider;
-        public bool isSomethingOnTop;
-        public int TileNumber;
-        //------------->CONSTRUCTORS<-------------//
-
-        public Tile(int tileNumber, Vector2 coordinates, int size) : base("Tile" + tileNumber, coordinates * size, new Vector2(size, size), 0f)
-        {
-            this.TileNumber = tileNumber;
-            this.Coordinates = coordinates;
-            this.isSomethingOnTop = false;
-            //this.isWalkable = true;
-            this.Collider = new Collider(coordinates * size, new Vector2(size, size));
-
-            //if(tileNumber == 1)
-            //{
-            //    isWalkable = false;
-            //}
-        }
-
-        //------------->FUNCTIONS && METHODS<-------------//
-
-        public void DrawTile(Camera camera)
-        {
-            this.Rectangle = camera.CalculatePixelRectangle(Position, Size);
-            Game1.spriteBatch.Draw(Texture, Rectangle, Color.White);
-        }
-
-        public bool isWalkable(Object unused)
-        {
-            if (this.TileNumber == 1)
-                return false;
-            else
-                return true;
-        }
-    }
-}
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;using Microsoft.Xna.Framework;using Microsoft.Xna.Framework.Graphics;using Microsoft.Xna.Framework.Content;namespace Projecto{    internal class Tile : GameObject, SettlersEngine.IPathNode<Object>    {        public Vector2 Coordinates;        public Collider Collider;        public bool isSomethingOnTop;        public int TileNumber;        //------------->CONSTRUCTORS<-------------//        public Tile(int tileNumber, Vector2 coordinates, int size) : base("Tile" + tileNumber, coordinates * size, new Vector2(size, size), 0f)        {            this.TileNumber = tileNumber;            this.Coordinates = coordinates;            this.isSomethingOnTop = false;            //this.isWalkable = true;            this.Collider = new Collider(coordinates * size, new Vector2(size, size));            //if(tileNumber == 1)            //{            //    isWalkable = false;            //}        }        //------------->FUNCTIONS && METHODS<-------------//        public void DrawTile(Camera camera)        {            this.Rectangle = camera.CalculatePixelRectangle(Position, Size);            Game1.spriteBatch.Draw(Texture, Rectangle, Color.White);        }        public bool isWalkable(Object unused)        {            if (this.TileNumber == 1)                return false;            else                return true;        }    }}
