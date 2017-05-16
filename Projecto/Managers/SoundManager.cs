1739026f9f5b4c38f7de302ead044359072af90a
Unknown <titi_mariano@hotmail.com>
2017-05-16 17:55:39 +0100

    Merge branch 'develop' into feature/Sound

--- Projecto/Managers/SoundManager.cs
+++ Projecto/Managers/SoundManager.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Media;
 
 namespace Projecto.Managers
 {
     class SoundManager
     {
+        bool songStart = false;
+        Song mainMenuTheme;
+        Song mainGameTheme;
+        // LoadContent method:
+        void LoadContent()
+        {
+            mainMenuTheme = content.load<Song>("mainMenuTheme");
+            mainGameTheme = content.load<Song>("mainGameTheme");
+            MediaPlayer.IsRepeating = true;
+        }
+
+        // Update method:
+        void UpdateSong()
+        {
+            switch (Game1.ScreenSelect)
+            {
+                case ScreenSelect.MainMenu:
+                    if (ScreenSelect == ScreenSelect.MainMenu && !songStart)
+                    {
+                        MediaPlayer.Stop();
+                        MediaPlayer.Play(mainMenuTheme);
+                        songStart = true;
+                    }
+                    else if (ScreenSelect != ScreenSelect.MainMenu && songStart == true)
+                    {
+                        MediaPlayer.Stop();
+                        songStart = false;
+                    }
+                    break;
+                case ScreenSelect.Playing:
+                    if (ScreenSelect == ScreenSelect.Playing && !songStart)
+                    {
+                        MediaPlayer.Stop();
+                        MediaPlayer.Play(mainGameTheme);
+                        songStart = true;
+                    }
+                    else if (ScreenSelect != ScreenSelect.Playing && songStart == true)
+                    {
+                        MediaPlayer.Stop();
+                        songStart = false;
+                    }
+                    break;
+            }
+        }
     }
 }
