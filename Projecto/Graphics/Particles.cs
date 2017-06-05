6668f2bb4071f8990eb82d2fac2c3ca9361438a3
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2017-06-05 02:49:19 +0100

    Fixin some stuff

--- Projecto/Graphics/Particles.cs
+++ Projecto/Graphics/Particles.cs
@@ -200,7 +200,7 @@ namespace Projecto
             }
             else if(Type == ParticleType.Smoke)
             {
-                PosFinal = new Vector2( Game1.random.Next((int)-SpawnRadius * 100, (int)SpawnRadius * 100) /100, Game1.random.Next((int)SpawnRadius * 100) / 100);
+                PosFinal = new Vector2( Game1.random.Next((int)-SpawnRadius * 100, (int)SpawnRadius * 100 + 1) /100, Game1.random.Next((int)SpawnRadius * 100) / 100);
                 DirFinal = Vector2.UnitY;
                 PosFinal += this.Position;
             }
